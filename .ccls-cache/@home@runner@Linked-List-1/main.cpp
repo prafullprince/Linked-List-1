76f54bed3019867d9c85405b092a668a76a2b2e7
Prafull Prince <159343572+prafullprince@users.noreply.github.com>
2024-07-22 07:11:02 +0000

    linked list creation

--- .ccls-cache/@home@runner@Linked-List-1/main.cpp
+++ .ccls-cache/@home@runner@Linked-List-1/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+//   cout<< "Hello World";
+
+//   1 ) Linked list node creation
+
+
+
+// class Node{
+//   public:
+//     int data;
+//     Node* next;
+
+//     Node(int value){
+//       this->data = value;
+//       this->next = NULL;
+//     }
+// };
+
+// int main() {
+//   Node* first = new Node(10);
+
+// }
+
+
+
+
+//     2 ) insertion
+
+
+class Node{
+  public:
+    int data;
+    Node* next;
+
+    Node(int value){
+      this->data = value;
+      this->next = NULL;
+    }
+};
+
 int main() {
-  cout<< "Hello World";
+  Node* first = new Node(10);
+
 }
