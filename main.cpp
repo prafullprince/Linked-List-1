c2481cc36344b65b076ac245c2db6a823ccc3bdf
Prafull Prince <159343572+prafullprince@users.noreply.github.com>
2024-07-22 07:48:51 +0000

    insertAtHead

--- main.cpp
+++ main.cpp
@@ -40,7 +40,45 @@ class Node{
     }
 };
 
-int main() {
-  Node* first = new Node(10);
+// print function for ll
+void print(Node* head){
+  Node* temp = head;
+  while(temp!=NULL){
+    cout << temp->data << "->";
+    temp = temp->next;
+  }
+  cout<<"NULL"<<endl;
+}
 
+// insert at head in ll
+Node* insertAtHead(int value,Node* &head,Node* &tail){
+  // ll is empty
+  if(head == NULL && tail == NULL){
+    // create a new node
+    Node* newNode = new Node(value);
+    // make new node is tail and head
+    head = newNode;
+    tail = newNode;
+  }
+  else{
+    // create a new node
+    Node* newNode = new Node(value);
+    // newNode ko head se connect kro
+    newNode->next = head;
+    // now make newnode to head
+    head = newNode;
+  }
+  return head;
+}
+
+int main() {
+  Node* head = NULL;
+  Node* tail = NULL;
+
+  insertAtHead(10,head,tail);
+  insertAtHead(20,head,tail);
+
+  print(head);
+  
+  
 }
